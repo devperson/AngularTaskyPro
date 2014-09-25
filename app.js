0c72bac0b9565aad237f68abea34e8e7c40182c2
Xusan <Csdafsd@gfgf.bb>
2014-09-26 03:26:34 +0500

    test2

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var express = require('express');
 var routes = require('./server/routes');
 var user = require('./server/routes/user');
+var apiRoutes = require('./server/api/TasksApi');
 var http = require('http');
 var path = require('path');
 
@@ -36,6 +37,12 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+
+//Api
+app.get('/api/tasks', apiRoutes.GetAllTasks);
+app.post('/api/tasks', apiRoutes.AddNewTask);
+app.put('/api/tasks', apiRoutes.EditTask);
+app.delete('/api/tasks', apiRoutes.RemoveTask);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
---
+++ server/api/TasksApi.js
@@ -0,0 +1,16 @@
+
+exports.GetAllTasks = function(req, res){
+    res.send("respond with a resource");
+};
+
+exports.AddNewTask = function(req, res){
+    res.send("respond with a resource");
+};
+
+exports.EditTask = function(req, res){
+    res.send("respond with a resource");
+};
+
+exports.RemoveTask = function(req, res){
+    res.send("respond with a resource");
+};
